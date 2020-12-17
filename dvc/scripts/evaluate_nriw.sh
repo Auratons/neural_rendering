aada06949a38e5f9128ae9a3897951b92da776fc
Tomas Kremel <kremelt@gmail.com>
2020-12-17 03:21:32 +0100

    Evaluation steps added to Hagia Sophia collection.

--- dvc/scripts/evaluate_nriw.sh
+++ dvc/scripts/evaluate_nriw.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=evaluate_nriw
+#SBATCH --job-name=evaluate_nriw_%j
 #SBATCH --output=evaluate_nriw_%j.log
 #SBATCH --mem=64G
 #SBATCH --time=0-01:00:00
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:4
+#SBATCH --cpus-per-gpu=16
 
 set -e
 
@@ -32,7 +33,7 @@ if [ -z ${TIMESTAMP} ]; then
 fi
 
 VIRTUAL_SEQ_NAME=$(cat params.yaml | yq -r '.evaluate_nriw_'$sub'.virtual_seq_name // "val"')
-TRAIN_DIR=$(cat params.yaml | yq -r '.evaluate_nriw_'$sub'.train_dir // (.evaluate_nriw_'$sub'.model_parent_dir + .evaluate_nriw_'$sub'.dataset_name | . += "-" | . += "'${TIMESTAMP}'-finetune_appearance/")')
+TRAIN_DIR=$(cat params.yaml | yq -r '.evaluate_nriw_'$sub'.train_dir // (.evaluate_nriw_'$sub'.model_parent_dir + .evaluate_nriw_'$sub'.dataset_name + "-'${TIMESTAMP}'-finetune_appearance/")')
 
 echo
 echo "Evaluating the validation set"
@@ -43,7 +44,7 @@ echo "   --dataset_parent_dir=$(cat params.yaml | yq -r '.evaluate_nriw_'$sub'.d
 echo "   --train_dir=${TRAIN_DIR}"
 echo "   --run_mode='eval_subset'"
 echo "   --virtual_seq_name=${VIRTUAL_SEQ_NAME}"
-echo "   --output_validation_dir=$(cat params.yaml | yq -r '.evaluate_nriw_'$sub'.output_validation_dir // "'${TRAIN_DIR}'evaluation-'${VIRTUAL_SEQ_NAME}'"')"
+echo "   --output_validation_dir=$(cat params.yaml | yq -r '.evaluate_nriw_'$sub'.output_validation_dir // (.evaluate_nriw_'$sub'.model_parent_dir + "../evaluations/" + .evaluate_nriw_'$sub'.dataset_name + "_'${TIMESTAMP}'_seq-'${VIRTUAL_SEQ_NAME}'")')"
 echo "   --use_buffer_appearance=$(cat params.yaml | yq -r '.evaluate_nriw_'$sub'.use_buffer_appearance // "True"')"
 echo "   --use_semantic_gt=$(cat params.yaml | yq -r '.evaluate_nriw_'$sub'.use_semantic_gt // "True"')"
 echo "   --use_semantic=$(cat params.yaml | yq -r '.evaluate_nriw_'$sub'.use_semantic // "True"')"
@@ -58,7 +59,7 @@ echo
     --train_dir=${TRAIN_DIR} \
     --run_mode='eval_subset' \
     --virtual_seq_name=${VIRTUAL_SEQ_NAME} \
-    --output_validation_dir=$(cat params.yaml | yq -r '.evaluate_nriw_'$sub'.output_validation_dir // "'${TRAIN_DIR}'evaluation-'${VIRTUAL_SEQ_NAME}'"') \
+    --output_validation_dir=$(cat params.yaml | yq -r '.evaluate_nriw_'$sub'.output_validation_dir // (.evaluate_nriw_'$sub'.model_parent_dir + "../evaluations/" + .evaluate_nriw_'$sub'.dataset_name + "_'${TIMESTAMP}'_seq-'${VIRTUAL_SEQ_NAME}'")') \
     --use_buffer_appearance=$(cat params.yaml | yq -r '.evaluate_nriw_'$sub'.use_buffer_appearance // "True"') \
     --use_semantic_gt=$(cat params.yaml | yq -r '.evaluate_nriw_'$sub'.use_semantic_gt // "True"') \
     --use_semantic=$(cat params.yaml | yq -r '.evaluate_nriw_'$sub'.use_semantic // "True"') \
@@ -71,12 +72,12 @@ echo
 echo "Evaluate quantitative metrics"
 echo "Running"
 echo "~/.linuxbrew/bin/time -f 'real\t%e s\nuser\t%U s\nsys\t%S s\nmemmax\t%M kB' python $WORKSPACE/evaluate_quantitative_metrics.py"
-echo "   --val_set_out_dir=$(cat params.yaml | yq -r '.evaluate_nriw_'$sub'.output_validation_dir // "'${TRAIN_DIR}'evaluation-'${VIRTUAL_SEQ_NAME}'"')"
-echo "   --experiment_title=$(cat params.yaml | yq -r '.evaluate_nriw_'$sub'.experiment_title // (.evaluate_nriw_'$sub'.dataset_name | . += "-'${VIRTUAL_SEQ_NAME}'")')"
+echo "   --val_set_out_dir=$(cat params.yaml | yq -r '.evaluate_nriw_'$sub'.output_validation_dir // (.evaluate_nriw_'$sub'.model_parent_dir + "../evaluations/" + .evaluate_nriw_'$sub'.dataset_name + "_'${TIMESTAMP}'_seq-'${VIRTUAL_SEQ_NAME}'")')"
+echo "   --experiment_title=$(cat params.yaml | yq -r '.evaluate_nriw_'$sub'.experiment_title // (.evaluate_nriw_'$sub'.dataset_name | . += "_'${TIMESTAMP}'_seq-'${VIRTUAL_SEQ_NAME}'")')"
 echo "   --logtostderr"
 echo
 
 ~/.linuxbrew/bin/time -f 'real\t%e s\nuser\t%U s\nsys\t%S s\nmemmax\t%M kB' python $WORKSPACE/evaluate_quantitative_metrics.py \
-    --val_set_out_dir=$(cat params.yaml | yq -r '.evaluate_nriw_'$sub'.output_validation_dir // "'${TRAIN_DIR}'evaluation-'${VIRTUAL_SEQ_NAME}'"') \
-    --experiment_title=$(cat params.yaml | yq -r '.evaluate_nriw_'$sub'.experiment_title // (.evaluate_nriw_'$sub'.dataset_name | . += "-'${VIRTUAL_SEQ_NAME}'")') \
+    --val_set_out_dir=$(cat params.yaml | yq -r '.evaluate_nriw_'$sub'.output_validation_dir // (.evaluate_nriw_'$sub'.model_parent_dir + "../evaluations/" + .evaluate_nriw_'$sub'.dataset_name + "_'${TIMESTAMP}'_seq-'${VIRTUAL_SEQ_NAME}'")') \
+    --experiment_title=$(cat params.yaml | yq -r '.evaluate_nriw_'$sub'.experiment_title // (.evaluate_nriw_'$sub'.dataset_name | . += "_'${TIMESTAMP}'_seq-'${VIRTUAL_SEQ_NAME}'")') \
     --logtostderr
