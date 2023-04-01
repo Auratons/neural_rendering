6f4d9f0a8e32c298ee9848c802ba78090683169d
Tomas Kremel <kremelt@gmail.com>
2023-04-01 14:46:11 +0200

    Cutouts generation Slurm script generalized to all datasets.

--- dvc/scripts/build_cutouts_inloc_algo.sh
+++ dvc/scripts/build_cutouts_inloc_algo.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=cutouts
 #SBATCH --output=logs/inloc_algo_cutouts_%j.log
 #SBATCH --mem=4G
-#SBATCH --time=0-02:00:00
+#SBATCH --time=0-04:00:00
 #SBATCH --partition=compute
 #SBATCH --cpus-per-task=2
 
@@ -20,28 +20,72 @@ echo "Running on $(hostname)"
 echo
 set -u
 
-echo
-echo "~/.homebrew/bin/time -f 'real\t%e s\nuser\t%U s\nsys\t%S s\nmemmax\t%M kB' singularity"
-echo "    exec --nv --bind /nfs:/nfs ~/containers/renderer-app.sif"
-echo "        ~/.conda/envs/pipeline/bin/python ~/inloc/inLocCIIRC_dataset/buildCutouts/build_cutouts_colmap.py"
-echo "        --input_root=$(cat params.yaml | yq -r '.inloc_cutouts_'$sub'.input_root')"
-echo "        --input_ply_path=$(cat params.yaml | yq -r '.inloc_cutouts_'$sub'.input_ply_path')"
-echo "        --input_root_colmap=$(cat params.yaml | yq -r '.inloc_cutouts_'$sub'.input_root_colmap')"
-echo "        --output_root=$(cat params.yaml | yq -r '.inloc_cutouts_'$sub'.output_root')"
-echo "        --test_size=$(cat params.yaml | yq -r '.inloc_cutouts_'$sub'.test_size')"
-echo "        --squarify=$(cat params.yaml | yq -r '.inloc_cutouts_'$sub'.squarify')"
-echo "        --val_ratio=$(cat params.yaml | yq -r '.inloc_cutouts_'$sub'.val_ratio // "0.2"')"
-echo
+if [[ "$(cat params.yaml | yq -r '.cpp_render_'$sub'.type // "COLMAP"')" == "COLMAP" ]]; then
+    echo
+    echo "~/.homebrew/bin/time -f 'real\t%e s\nuser\t%U s\nsys\t%S s\nmemmax\t%M kB' singularity"
+    echo "    exec --nv --bind /nfs:/nfs ~/containers/renderer-app.sif"
+    echo "        ~/.conda/envs/pipeline/bin/python ~/inloc/inLocCIIRC_dataset/buildCutouts/build_cutouts_colmap.py"
+    echo "        --input_root=$(cat params.yaml | yq -r '.inloc_cutouts_'$sub'.input_root')"
+    echo "        --input_ply_path=$(cat params.yaml | yq -r '.inloc_cutouts_'$sub'.input_ply_path')"
+    echo "        --input_root_colmap=$(cat params.yaml | yq -r '.inloc_cutouts_'$sub'.input_root_colmap')"
+    echo "        --output_root=$(cat params.yaml | yq -r '.inloc_cutouts_'$sub'.output_root')"
+    echo "        --test_size=$(cat params.yaml | yq -r '.inloc_cutouts_'$sub'.test_size')"
+    echo "        --squarify=$(cat params.yaml | yq -r '.inloc_cutouts_'$sub'.squarify')"
+    echo "        --val_ratio=$(cat params.yaml | yq -r '.inloc_cutouts_'$sub'.val_ratio // "0.2"')"
+    echo "        --input_root_renderer=$(cat params.yaml | yq -r '.inloc_cutouts_'$sub'.input_root_renderer // "pyrender"')"
+    echo
 
-~/.homebrew/bin/time -f 'real\t%e s\nuser\t%U s\nsys\t%S s\nmemmax\t%M kB' singularity \
-    exec --bind /nfs:/nfs ~/containers/renderer-app.sif \
-        ~/.conda/envs/pipeline/bin/python ~/inloc/inLocCIIRC_dataset/buildCutouts/build_cutouts_colmap.py \
-        --input_root=$(cat params.yaml | yq -r '.inloc_cutouts_'$sub'.input_root') \
-        --input_ply_path=$(cat params.yaml | yq -r '.inloc_cutouts_'$sub'.input_ply_path') \
-        --input_root_colmap=$(cat params.yaml | yq -r '.inloc_cutouts_'$sub'.input_root_colmap') \
-        --output_root=$(cat params.yaml | yq -r '.inloc_cutouts_'$sub'.output_root') \
-        --test_size=$(cat params.yaml | yq -r '.inloc_cutouts_'$sub'.test_size') \
-        --squarify=$(cat params.yaml | yq -r '.inloc_cutouts_'$sub'.squarify') \
-        --val_ratio=$(cat params.yaml | yq -r '.inloc_cutouts_'$sub'.val_ratio // "0.2"')
+    ~/.homebrew/bin/time -f 'real\t%e s\nuser\t%U s\nsys\t%S s\nmemmax\t%M kB' singularity \
+        exec --bind /nfs:/nfs ~/containers/renderer-app.sif \
+            ~/.conda/envs/pipeline/bin/python ~/inloc/inLocCIIRC_dataset/buildCutouts/build_cutouts_colmap.py \
+            --input_root=$(cat params.yaml | yq -r '.inloc_cutouts_'$sub'.input_root') \
+            --input_ply_path=$(cat params.yaml | yq -r '.inloc_cutouts_'$sub'.input_ply_path') \
+            --input_root_colmap=$(cat params.yaml | yq -r '.inloc_cutouts_'$sub'.input_root_colmap') \
+            --output_root=$(cat params.yaml | yq -r '.inloc_cutouts_'$sub'.output_root') \
+            --test_size=$(cat params.yaml | yq -r '.inloc_cutouts_'$sub'.test_size') \
+            --squarify=$(cat params.yaml | yq -r '.inloc_cutouts_'$sub'.squarify') \
+            --val_ratio=$(cat params.yaml | yq -r '.inloc_cutouts_'$sub'.val_ratio // "0.2"') \
+            --input_root_renderer=$(cat params.yaml | yq -r '.inloc_cutouts_'$sub'.input_root_renderer // "pyrender"')
+fi
+
+
+if [[ "$(cat params.yaml | yq -r '.cpp_render_'$sub'.type // "COLMAP"')" == "ARTWIN" ]]; then
+    echo
+    echo "~/.homebrew/bin/time -f 'real\t%e s\nuser\t%U s\nsys\t%S s\nmemmax\t%M kB' singularity"
+    echo "    exec --nv --bind /nfs:/nfs ~/containers/renderer-app.sif"
+    echo "        ~/.conda/envs/pipeline/bin/python ~/inloc/inLocCIIRC_dataset/buildCutouts/build_cutouts_artwin.py"
+    echo "        --input_root=$(cat params.yaml | yq -r '.inloc_cutouts_'$sub'.input_root')"
+    echo "        --output_root=$(cat params.yaml | yq -r '.inloc_cutouts_'$sub'.output_root')"
+    echo "        --input_mapping=$(cat params.yaml | yq -r '.inloc_cutouts_'$sub'.input_mapping')"
+    echo "        --input_root_renderer=$(cat params.yaml | yq -r '.inloc_cutouts_'$sub'.input_root_renderer // "pyrender"')"
+    echo
+
+    ~/.homebrew/bin/time -f 'real\t%e s\nuser\t%U s\nsys\t%S s\nmemmax\t%M kB' singularity \
+        exec --bind /nfs:/nfs ~/containers/renderer-app.sif \
+            ~/.conda/envs/pipeline/bin/python ~/inloc/inLocCIIRC_dataset/buildCutouts/build_cutouts_artwin.py \
+            --input_root=$(cat params.yaml | yq -r '.inloc_cutouts_'$sub'.input_root') \
+            --output_root=$(cat params.yaml | yq -r '.inloc_cutouts_'$sub'.output_root') \
+            --input_mapping=$(cat params.yaml | yq -r '.inloc_cutouts_'$sub'.input_mapping') \
+            --input_root_renderer=$(cat params.yaml | yq -r '.inloc_cutouts_'$sub'.input_root_renderer // "pyrender"')
+fi
+
+
+if [[ "$(cat params.yaml | yq -r '.cpp_render_'$sub'.type // "COLMAP"')" == "INLOC" ]]; then
+    echo
+    echo "~/.homebrew/bin/time -f 'real\t%e s\nuser\t%U s\nsys\t%S s\nmemmax\t%M kB' singularity"
+    echo "    exec --nv --bind /nfs:/nfs ~/containers/renderer-app.sif"
+    echo "        ~/.conda/envs/pipeline/bin/python ~/inloc/inLocCIIRC_dataset/buildCutouts/build_cutouts_inloc.py"
+    echo "        --input_root=$(cat params.yaml | yq -r '.inloc_cutouts_'$sub'.input_root')"
+    echo "        --output_root=$(cat params.yaml | yq -r '.inloc_cutouts_'$sub'.output_root')"
+    echo "        --input_root_renderer=$(cat params.yaml | yq -r '.inloc_cutouts_'$sub'.input_root_renderer // "pyrender"')"
+    echo
+
+    ~/.homebrew/bin/time -f 'real\t%e s\nuser\t%U s\nsys\t%S s\nmemmax\t%M kB' singularity \
+        exec --bind /nfs:/nfs ~/containers/renderer-app.sif \
+            ~/.conda/envs/pipeline/bin/python ~/inloc/inLocCIIRC_dataset/buildCutouts/build_cutouts_inloc.py \
+            --input_root=$(cat params.yaml | yq -r '.inloc_cutouts_'$sub'.input_root') \
+            --output_root=$(cat params.yaml | yq -r '.inloc_cutouts_'$sub'.output_root') \
+            --input_root_renderer=$(cat params.yaml | yq -r '.inloc_cutouts_'$sub'.input_root_renderer // "pyrender"')
+fi
 
 exit 0
